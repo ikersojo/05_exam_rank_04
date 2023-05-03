155abc54db9e4f2b035ac5be6cfbcc418c8b6bc6
Iker Sojo <105147105+ikersojo@users.noreply.github.com>
2023-05-03 11:12:52 +0000

    baseline solution

--- microshell.c
+++ microshell.c
@@ -1,56 +1,86 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   microshell.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/11 15:10:10 by isojo-go          #+#    #+#             */
-/*   Updated: 2023/03/11 15:29:53 by isojo-go         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
-#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
 
-typedef struct s_cmd
+int	ft_putstr_fd2(char *str, char *arg)
 {
-	char			*str;
-	int				infd;
-	int				outfd;
-	struct s_cmd	*next;
-	struct s_cmd	*prev;
-}					t_cmd;
-
-typedef struct s_data
-{
-	int		base_stdin;
-	int		base_stdout;
-	int		status;
-	t_cmd	*cmd;
-}			t_data;
-
-t_data	*ft_init_data(t_data *data)
-{
-	int	i;
-
-	data = (t_data *)malloc(sizeof(data));
-	if (!data)
-		return (NULL);
-	data->base_stdin = dup(STDIN_FILENO);
-	data->base_stdout = dup(STDOUT_FILENO);
-	data->status = 0;
-	data->cmd = NULL;
+	while (*str)
+		write(2, str++, 1);
+	if (arg)
+		while(*arg)
+			write(2, arg++, 1);
+	write(2, "\n", 1);
+	return (1);
 }
 
-int	main(int argc, char **argv, char **envp)
+int ft_execute(char *argv[], int i, int tmp_fd, char *env[])
 {
-	t_data	*data;
+	//overwrite ; or | or NULL with NULL to use the array as input for execve.
+	//we are here in the child so it has no impact in the parent process.
+	argv[i] = NULL;
+	dup2(tmp_fd, STDIN_FILENO);
+	close(tmp_fd);
+	execve(argv[0], argv, env);
+	return (ft_putstr_fd2("error: cannot execute ", argv[0]));
+}
 
-	if (argc == 1)
-		return (EXIT_SUCCESS);
-	data = ft_init_data(data);
-	ft_parse(data, argv);
-	ft_exec(data, envp);
-	return(data->status);
+int	main(int argc, char *argv[], char *env[])
+{
+	int	i;
+	int fd[2];
+	int tmp_fd;
+	(void)argc;	// is needed in exam, because the exam tester compiles with -Wall -Wextra -Werror
+
+	i = 0;
+	tmp_fd = dup(STDIN_FILENO);
+	while (argv[i] && argv[i + 1]) //check if the end is reached
+	{
+		argv = &argv[i + 1];	//the new argv start after the ; or |
+		i = 0;
+		//count until we have all informations to execute the next child;
+		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+			i++;
+		if (strcmp(argv[0], "cd") == 0) //cd
+		{
+			if (i != 2)
+				ft_putstr_fd2("error: cd: bad arguments", NULL);
+			else if (chdir(argv[1]) != 0)
+				ft_putstr_fd2("error: cd: cannot change directory to ", argv[1]	);
+		}
+		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) //exec in stdout
+		{
+			if ( fork() == 0)
+			{
+				if (ft_execute(argv, i, tmp_fd, env))
+					return (1);
+			}
+			else
+			{
+				close(tmp_fd);
+				while(waitpid(-1, NULL, WUNTRACED) != -1)
+					;
+				tmp_fd = dup(STDIN_FILENO);
+			}
+		}
+		else if(i != 0 && strcmp(argv[i], "|") == 0) //pipe
+		{
+			pipe(fd);
+			if ( fork() == 0)
+			{
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+				if (ft_execute(argv, i, tmp_fd, env))
+					return (1);
+			}
+			else
+			{
+				close(fd[1]);
+				close(tmp_fd);
+				tmp_fd = fd[0];
+			}
+		}
+	}
+	close(tmp_fd);
+	return (0);
 }
