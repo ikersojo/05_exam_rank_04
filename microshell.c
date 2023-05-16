ec2594ad6fccc6f8e1ccf66fe51f6618b74894aa
Iker Sojo <ikersojo@hotmail.com>
2023-05-16 10:09:27 +0200

    update

--- microshell.c
+++ microshell.c
@@ -1,96 +1,100 @@
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
 
-/*not needed in exam, but necessary if you want to use this tester:
-https://github.com/Glagan/42-exam-rank-04/blob/master/microshell/test.sh*/
-// #ifdef TEST_SH
-// # define TEST		1
-// #else
-// # define TEST		0
-// #endif
+void	ft_putchar_fd2(char c)
+{
+	write(2, &c, 1);
+}
 
-int	ft_putstr_fd2(char *str, char *arg)
+void	ft_putstr_fd2(char *str)
 {
 	while (*str)
-		write(2, str++, 1);
+		ft_putchar_fd2(*str++);
+}
+
+int	ft_puterror(char *str, char *arg)
+{
+	ft_putstr_fd2(str);
 	if (arg)
-		while(*arg)
-			write(2, arg++, 1);
-	write(2, "\n", 1);
+		ft_putstr_fd2(arg);
+	ft_putchar_fd2('\n');
 	return (1);
 }
 
-int ft_execute(char *argv[], int i, int tmp_fd, char *env[])
+int	ft_exec(char **argv, char **env, int i, int tempfd)
 {
-	//overwrite ; or | or NULL with NULL to use the array as input for execve.
-	//we are here in the child so it has no impact in the parent process.
 	argv[i] = NULL;
-	dup2(tmp_fd, STDIN_FILENO);
-	close(tmp_fd);
+	dup2(tempfd, STDIN_FILENO);
+	close (tempfd);
 	execve(argv[0], argv, env);
-	return (ft_putstr_fd2("error: cannot execute ", argv[0]));
+	return (ft_puterror("error: cannot execute ", argv[0]));
 }
 
-int	main(int argc, char *argv[], char *env[])
+int	main(int argc, char **argv, char ** env)
 {
-	int	i;
-	int fd[2];
-	int tmp_fd;
-	(void)argc;	// is needed in exam, because the exam tester compiles with -Wall -Wextra -Werror
+	int		tempfd;
+	int		i;
+	int		fd[2];
+	pid_t	pid;
 
+	(void)argc;
+	tempfd = dup(STDIN_FILENO);
 	i = 0;
-	tmp_fd = dup(STDIN_FILENO);
-	while (argv[i] && argv[i + 1]) //check if the end is reached
+	while (argv[i] && argv[i + 1])
 	{
-		argv = &argv[i + 1];	//the new argv start after the ; or |
+		argv = &argv[i + 1];
 		i = 0;
-		//count until we have all informations to execute the next child;
 		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
 			i++;
-		if (strcmp(argv[0], "cd") == 0) //cd
+		if (strcmp(argv[0], "cd") == 0)
 		{
 			if (i != 2)
-				ft_putstr_fd2("error: cd: bad arguments", NULL);
+				ft_puterror("error: cd: bad arguments", NULL);
 			else if (chdir(argv[1]) != 0)
-				ft_putstr_fd2("error: cd: cannot change directory to ", argv[1]	);
+				ft_puterror("error: cd: cannot change directory to ", argv[1]);
 		}
-		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) //exec in stdout
+		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
 		{
-			if ( fork() == 0)
+			pid = fork();
+			if (pid == -1)
+				ft_puterror("error: fatal", NULL);
+			else if (pid == 0)
 			{
-				if (ft_execute(argv, i, tmp_fd, env))
+				if (ft_exec(argv, env, i, tempfd) == 1)
 					return (1);
 			}
 			else
 			{
-				close(tmp_fd);
-				while(waitpid(-1, NULL, WUNTRACED) != -1)
+				close (tempfd);
+				while (waitpid(-1, NULL, WUNTRACED) != -1)
 					;
-				tmp_fd = dup(STDIN_FILENO);
+				tempfd = dup(STDIN_FILENO);
 			}
 		}
-		else if(i != 0 && strcmp(argv[i], "|") == 0) //pipe
+		else if (i != 0 && strcmp(argv[i], "|") == 0)
 		{
-			pipe(fd);
-			if ( fork() == 0)
+			if (pipe(fd) == -1)
+				ft_puterror("error: fatal", NULL);
+			pid = fork();
+			if (pid == -1)
+				ft_puterror("error: fatal", NULL);
+			else if (pid == 0)
 			{
 				dup2(fd[1], STDOUT_FILENO);
-				close(fd[0]);
-				close(fd[1]);
-				if (ft_execute(argv, i, tmp_fd, env))
+				close (fd[0]);
+				close (fd[1]);
+				if (ft_exec(argv, env, i, tempfd) == 1)
 					return (1);
 			}
 			else
 			{
+				close (tempfd);
 				close(fd[1]);
-				close(tmp_fd);
-				tmp_fd = fd[0];
+				tempfd = fd[0];
 			}
 		}
 	}
-	close(tmp_fd);
-	// if (TEST)		// not needed in exam, but necessary if you want to use this tester:
-	// 	while (1);	// https://github.com/Glagan/42-exam-rank-04/blob/master/microshell/test.sh
+	close (tempfd);
 	return (0);
 }
--- subject.md
+++ subject.md
@@ -1,7 +1,9 @@
-Assignment name  : microshell
-Expected files   : *.c *.h
-Allowed functions: malloc, free, write, close, fork, waitpid, signal, kill, exit, chdir, execve, dup, dup2, pipe, strcmp, strncmp
---------------------------------------------------------------------------------------
+# exam_rank_04 subject
+---
+- **Assignment name  :** microshell
+- **Expected files   :** *.c *.h
+- **Allowed functions:** malloc, free, write, close, fork, waitpid, signal, kill, exit, chdir, execve, dup, dup2, pipe, strcmp, strncmp
+---
 
 Write a program that will behave like executing a shell command
 - The command line to execute will be the arguments of this program
@@ -19,13 +21,16 @@ Write a program that will behave like executing a shell command
 - Your program should be able to manage more than hundreds of "|" even if we limit the number of "open files" to less than 30.
 
 for example this should work:
+```sh
 $>./microshell /bin/ls "|" /usr/bin/grep microshell ";" /bin/echo i love my microshell
+
 microshell
 i love my microshell
+
 $>
 
-Hints:
-Don't forget to pass the environment variable to execve
+```
 
-Hints:
-Do not leak file descriptors!
+**Hints:**
+- *Don't forget to pass the environment variable to execve*
+- *Do not leak file descriptors!*
